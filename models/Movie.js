8e1ab114f602b4b07a0805024a0a912b20b334da
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-19 08:34:36 +0530

    MRS

---
+++ models/Movie.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    director: {
+        type: String,
+        required: true,
+    },
+    releaseYear: {
+        type: Number,
+        required: true,
+    },
+    genre: {
+        type: String,
+        required: true,
+    },
+    reviews: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Review',
+        },
+    ],
+});
+
+module.exports = mongoose.model('Movie', movieSchema);
